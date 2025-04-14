c03c8440d7f83d39510281a0a04614c0a4892724
aryamangupta04 <66553795+aryamangupta04@users.noreply.github.com>
2025-04-14 17:22:35 +0530

    changes

--- base/views.py
+++ base/views.py
@@ -70,9 +70,9 @@ def profile(request,pk):
 
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    room_messages=room.message_set.all().order_by('-created')
+    room_messages=room.message_set.all().order_by('created')
     
-    print(room.participants.all())  # Debugging line
+    print(room.participants.all()) 
     if request.method=="POST":
         message=Message.objects.create(
             user=request.user,
